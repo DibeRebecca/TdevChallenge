09689e60348eaa0d34e091cf2520048e01e93fe3
DibeRebecca <rebe.taboukouna@gmail.com>
2021-11-25 21:09:43 +0200

    Day9

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ DAY 5 Page de Detail d un article
 DAY 6 Interface de la page de panier
 DAY 7 Inscription des utilisateurs et enregistrement dans la base de donnee
 DAY 8 Creation des categories, Marques, Commandes et ajout dans la Base de Donnee
+DAY 9 Ajout des articles dans la base de donnee
 # ecommerce
 
 >>>>>>> 73c46025af8fc94b38b24ee36999a7a668a759a8
---
+++ lib/screens/otp/components/body.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/size_config.dart';
+
+import 'otp_form.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: SizeConfig.screenHeight * 0.05),
+              Text(
+                " Verification OPT",
+                style: headingStyle,
+              ),
+              Text("Nous avons envoye un code a +228 97 86 ****"),
+              buildTimer(),
+              OtpForm(),
+              SizedBox(height: SizeConfig.screenHeight * 0.1),
+              GestureDetector(
+                onTap: () {
+                  // OTP code resend
+                },
+                child: Text(
+                  "Renvoyer le Code",
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row buildTimer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Ce code expire dans "),
+        TweenAnimationBuilder(
+          tween: Tween(begin: 30.0, end: 0.0),
+          duration: Duration(seconds: 30),
+          builder: (_, dynamic value, child) => Text(
+            "00:${value.toInt()}",
+            style: TextStyle(color: kPrimaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
